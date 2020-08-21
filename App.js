2eb65d1ad5c5345b9cfae418e66df3b87505219f
zstuder25 <zstuder25@gmail.com>
2020-08-21 17:09:27 -0400

    init commit

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, Button } from 'react-native';
+import GoalItem from "./GoalItem";
+import GoalInput from "./GoalInput";
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+  const addGoalHandler = (goalTitle) => {
+    setCourseGoals(currentGoals => [...courseGoals, {uid: Math.random().toString(), value: goalTitle}])
+    setIsAddMode(false);
+  }
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goal.uid !== goalId)
+    })
+  }
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput onAddGoal={addGoalHandler} onCancel={cancelGoalAdditionHandler} visible={isAddMode} />
+      <FlatList
+          keyExtractor={(item, index) => item.uid}
+          data={courseGoals}
+          renderItem={itemData => (
+            <GoalItem onDelete={() => removeGoalHandler(itemData.item.uid)} title={itemData.item.value} />
+          )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  screen: {
+    padding: 50
+  }
+})
---
+++ GoalItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Text, View, StyleSheet, TouchableOpacity } from "react-native";
+
+const GoalItem = props => {
+    return (
+        <TouchableOpacity activeOpacity={0.8} onPress={props.onDelete} >
+            <View style={styles.listItem}>
+                <Text>{props.title}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    listItem: {
+        padding: 10,
+        marginVertical: 10,
+        backgroundColor: 'gray',
+        borderColor: 'black',
+        borderWidth: 1
+    }
+})
+
+export default GoalItem;
